bbedad4bda10965c5fe104bbea9de9c8f97b4857
chris lecompte & jordan <student@epicodus.com>
2015-06-17 16:42:01 -0700

    add math stuff

--- app/controllers/new-rating.js
+++ app/controllers/new-rating.js
@@ -13,6 +13,15 @@ export default Ember.Controller.extend({
 
       var cart = this.get('controllers.cart.model');
       cart.get('ratings').pushObject(newRating);
+
+      var ratings = cart.get('ratings');
+      var ratingTotal = 0;
+      raings.forEach(function(rating) {
+        ratingTotal+=rating.rating;
+      });
+      ratingTotal /= ratings.length;
+      cart.ratingTotal.set('ratingTotal', ratingTotal);
+
       cart.save();
 
       this.set('author', '');
--- app/models/cart.js
+++ app/models/cart.js
@@ -6,5 +6,5 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   foodType: DS.attr('string'),
   ratings: DS.hasMany('rating', {async: true}),
-  waitTime: DS.attr('string')
+  waitTime: DS.attr('string'),
 });
